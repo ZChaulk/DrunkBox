5a127f281e87be7d0baf2acefe3d626c9babaa20
mrBlue16 <zackary.chaulk@gmail.com>
2018-01-27 21:39:59 -0330

    Added check for valid number of players

--- app/controllers/game_controller.rb
+++ app/controllers/game_controller.rb
@@ -1,11 +1,22 @@
 class GameController < ApplicationController
   def new
-    @game = Game.new(params.require(:game).permit(:gameName, :playerCount))
+    @game = Game.new(new_game_params)
 
-    if @game[:gameName] == "pkmon"
-      redirect_to game_pkmon_path
+    if @game[:playerCount] == nil || @game[:playerCount] <= 0
+      redirect_to welcome_game_path :action => "error",  alert: "Invalid Value for player count!"
+    else
+      if @game[:gameName] == "pkmon"
+        redirect_to game_pkmon_path :action => "start", playerCount: @game[:playerCount]
+      end
     end
   end
+
   def pkmon
+
+  end
+
+  private
+  def new_game_params
+    params.require(:game).permit(:gameName, :playerCount)
   end
 end
